92c9397f73bfbd0ef59575c8e82dd6d0e9d62bba
FavorLive <Favor.hotmail@gmail.com>
2020-12-14 23:51:55 +0100

    création du formulaire et mise en forme

--- projet-code/projet-da - 0.1.php
+++ projet-code/projet-da - 0.1.php
@@ -2,17 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>Your first HTML form, styled</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Projet - Déclararer un camp </title>
 		<link rel="stylesheet" href="da-style-centralized.css">
 
 	</head>
 
 	<body>
+
         <h1> Déclaration d'activité </h1>
         <h2> Identification du Pouvoir organisateur </h2>
-
-
-
 
 		<form action="/my-handling-form-page" method="post">
 			<ul>
@@ -20,17 +19,8 @@
 					<label for="name">Dénomination:</label>
 					<input type="text" id="name" name="user_name" />
                 </li>
-                
-                
-
-
-
-
-
-
-
-
-				<li>
+                <h3> test </h3>
+             	<li>
 					<label for="street">E-mail:</label>
 					<input type="email" id="mail" name="user_mail" />
 				</li>
@@ -43,5 +33,107 @@
 				</li>
 			</ul>
 		</form>
+
+
+	    <h2> Compte financier </h2>
+
+		<form action="/my-handling-form-page" method="post">
+			<ul>
+				<li>
+					<label for="name">Dénomination:</label>
+					<input type="text" id="name" name="user_name" />
+                </li>
+                
+             	<li>
+					<label for="street">E-mail:</label>
+					<input type="email" id="mail" name="user_mail" />
+				</li>
+				<li>
+					<label for="msg">Message:</label>
+					<textarea id="msg" name="user_message"></textarea>
+				</li>
+				<li class="button">
+					<button type="submit">Send your message</button>
+				</li>
+			</ul>
+		</form>	
+
+
+
+	    <h2> Estimation nombre d'enfants et d'encadrants  </h2>
+
+		<form action="/my-handling-form-page" method="post">
+			<ul>
+				<li>
+					<label for="name">Dénomination:</label>
+					<input type="text" id="name" name="user_name" />
+                </li>
+                
+             	<li>
+					<label for="street">E-mail:</label>
+					<input type="email" id="mail" name="user_mail" />
+				</li>
+				<li>
+					<label for="msg">Message:</label>
+					<textarea id="msg" name="user_message"></textarea>
+				</li>
+				<li class="button">
+					<button type="submit">Send your message</button>
+				</li>
+			</ul>
+		</form>	
+
+
+	    <h2> Centre de vacances </h2>
+
+		<form action="/my-handling-form-page" method="post">
+			<ul>
+				<li>
+					<label for="name">Dénomination:</label>
+					<input type="text" id="name" name="user_name" />
+                </li>
+                
+             	<li>
+					<label for="street">E-mail:</label>
+					<input type="email" id="mail" name="user_mail" />
+				</li>
+				<li>
+					<label for="msg">Message:</label>
+					<textarea id="msg" name="user_message"></textarea>
+				</li>
+				<li class="button">
+					<button type="submit">Send your message</button>
+				</li>
+			</ul>
+		</form>	
+
+
+
+
+	    <h2> Coordinateur du centre - Responsable qualifié (pour les camps) </h2>
+
+		<form action="/my-handling-form-page" method="post">
+			<ul>
+				<li>
+					<label for="name">Dénomination:</label>
+					<input type="text" id="name" name="user_name" />
+                </li>
+                
+             	<li>
+					<label for="street">E-mail:</label>
+					<input type="email" id="mail" name="user_mail" />
+				</li>
+				<li>
+					<label for="msg">Message:</label>
+					<textarea id="msg" name="user_message"></textarea>
+				</li>
+				<li class="button">
+					<button type="submit">Send your message</button>
+				</li>
+			</ul>
+		</form>	
+
+
+
 	</body>
 </html>
